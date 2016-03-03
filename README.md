274ad5527a904edfc3024571e45457e6c2d341c9
Miha Sedej <miha.sedej@gmail.com>
2016-03-03 22:26:06 +0100

    Chart
    First chart, data is loaded on startup

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ## OxyPlot charts working example
 
-I went through a lot of problems when trying to compile a working WPF application with OxyPlot. To maybe save some problems to other programmers I am publishing this.
+I went through a lot of problems when trying to compile a working WPF application with OxyPlot. To maybe save some problems to other programmers, I am publishing this.
+
+There were some examples available, but many of more complex-ones were not compiling with newest version of OxyPlot.Wpf NuGet package.
+
+## Steps
+
+1. Created plain WPF application
+2. Through NuGet packet manager I added OxyPlot.Wpf package (don't forget "Include prerelease" tick when searching)
---
+++ ViewModel.cs
@@ -0,0 +1,65 @@
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OxyPlot_charts_working_example
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        private PlotModel plotModel;
+
+        public PlotModel PlotModel
+        {
+            get { return plotModel; }
+            set { plotModel = value; OnPropertyChanged("PlotModel"); }
+        }
+
+        public ViewModel()
+        {
+            PlotModel = new PlotModel();
+            SetUpModel();
+            LoadData();
+
+        }
+
+        private void SetUpModel()
+        {
+            PlotModel.Title = "AreaSeries with default style";
+
+            var linearAxis1 = new LinearAxis();
+            linearAxis1.Position = AxisPosition.Bottom;
+            PlotModel.Axes.Add(linearAxis1);
+
+            var linearAxis2 = new LinearAxis();
+            PlotModel.Axes.Add(linearAxis2);
+
+        }
+
+        public void LoadData()
+        {
+            var areaSeries1 = new AreaSeries();
+            areaSeries1.Points2.Add(new DataPoint(0, 60));
+            areaSeries1.Points2.Add(new DataPoint(5, 80));
+            areaSeries1.Points2.Add(new DataPoint(20, 70));
+            areaSeries1.Points.Add(new DataPoint(0, 50));
+            areaSeries1.Points.Add(new DataPoint(10, 40));
+            areaSeries1.Points.Add(new DataPoint(20, 60));
+            PlotModel.Series.Add(areaSeries1);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
